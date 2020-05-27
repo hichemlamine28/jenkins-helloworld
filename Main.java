0099bce1d68c67507c995e6140aba222eb94644f
Hichem Elamine <35197290+hichemlamine28@users.noreply.github.com>
2020-05-28 01:49:32 +0200

    modification de contenu 28/05/2020
    ok

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world, Bienvenu à la formation jenkins!");
+        System.out.println("Bienvenu à la formation jenkins! CI/CD devient plus clair ! non ??");
     }
 }
 
