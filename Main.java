af1b253bc15eb21d45f4673d4136688b1439f739
Hichem Elamine <Hichem.elamine@breakpoint-technology.fr>
2020-09-16 14:56:57 +0100

    modify version 1.2 in main

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenu à la formation jenkins! CI/CD devient plus clair ! non ??");
-	System.out.println("Tag v1.1");
+	System.out.println("Tag v1.2");
     }
 }
 
