229dcd0651e4f45ad1d5d50ae358708b1fee2b80
Hichem Elamine <Hichem.elamine@breakpoint-technology.fr>
2020-09-18 12:28:33 +0100

    modif main 1.4  and 1.5

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenu à la formation jenkins! CI/CD devient plus clair ! non ??");
-	System.out.println("Tag v1.3");
+	System.out.println("Tag v1.4");
+	System.out.println("Tag v1.5 added");
     }
 }
 
